4bbdaf14d46698a774a97a0611a46ce2e4408c52
Michelle Kaiser <whalen.michelle@gmail.com>
2018-05-23 09:17:04 -0400

    First File
    This file is here to make a space just to experiment with GitHub features.  All are welcome.

---
+++ graffiti.py
@@ -0,0 +1,5 @@
+//project started April 23, 2018
+
+print ("Michelle Kaiser was here on April 23, 2018.  She initialized this project, to have a space to make mistakes and learn.")
+
+print("change this print statement")
